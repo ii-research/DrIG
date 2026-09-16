382b1d887462f372e70ca852cd64297e926c837d
Kaipeng Li <likaipeng666@gmail.com>
2026-09-16 16:34:27 +0900

    Updated

--- src/models/residual_quantization/configs_scripts/run_train.sh
+++ src/models/residual_quantization/configs_scripts/run_train.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-genir_dir="/home/iiserver31/Workbench/likaipeng/dig"
+genir_dir="/home/"
 SRC="$genir_dir/src"
-mbeir_data_dir="/data/likaipeng/M-BEIR"
+mbeir_data_dir="/data/M-BEIR"
 
 export PYTHONPATH="$SRC"
 export CUDA_VISIBLE_DEVICES=0,1,2,3
